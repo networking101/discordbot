e32a9021261249eec89810cf6870ccf3b88b0d9b
networking101 <kowpak14@gmail.com>
2020-05-08 13:56:22 -0400

    fixed echo

--- botEcho.js
+++ botEcho.js
@@ -1,6 +1,22 @@
 exports.echoMessage = function(client, message, channel) {
     newMessage = {}
+
     newMessage["content"] = message.content
+
+    if (newMessage["content"].toLowerCase().includes("yub nub")){
+        let newID = client.users.cache.find(user => user.id === message.author.id)
+        newMessage["content"] = newMessage["content"].replace(/yub nub/gi, newID)
+    }
+    if (newMessage["content"].toLowerCase().includes("michael")){
+        let newID = client.users.cache.find(user => user.id === message.author.id)
+        newMessage["content"] = newMessage["content"].replace(/michael kowpak/gi, newID)
+        newMessage["content"] = newMessage["content"].replace(/michael/gi, newID)
+    }
+    if (newMessage["content"].toLowerCase().includes("kowpak")){
+        let newID = client.users.cache.find(user => user.id === message.author.id)
+        newMessage["content"] = newMessage["content"].replace(/michael kowpak/gi, newID)
+        newMessage["content"] = newMessage["content"].replace(/kowpak/gi, newID)
+    }
 
     if (message.attachments.first() != null){
         newMessage["files"] = [message.attachments.first().url]
